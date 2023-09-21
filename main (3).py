73e08e8edefccff583f3842d2dbffd7795d9915a
rasimuni93 <144541991+rasimuni93@users.noreply.github.com>
2023-09-21 21:33:44 +0530

    Unit 3-challenge3.1

---
+++ main (3).py
@@ -0,0 +1,13 @@
+def linearsearchproduct(productlist,targetproduct):
+  indices=[]
+  for index, product in enumerate(productlist):
+    if product == targetproduct:    
+      indices.append(index)  
+      
+  return indices
+
+# Example usage:
+products=["shoes","boot","loofer","shoes","sandal","shoes"]
+target="shoes"
+result=linearsearchproduct(products,target)
+print(result)
